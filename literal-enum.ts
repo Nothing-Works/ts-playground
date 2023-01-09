5161814e19e04884349e3b68928c3a27376f9eaa
AndySong <andylauszp@gmail.com>
2023-01-10 00:28:54 +1300

    literal-enum

---
+++ literal-enum.ts
@@ -0,0 +1,23 @@
+// Literal type
+const flipCoin = (): "Head" | "Tail" => (Math.random() < 0.5 ? "Head" : "Tail");
+
+console.log(flipCoin());
+
+// Enum
+enum Suit {
+  HEARTS,
+  SPADES,
+  DIAMONDS,
+  CLUBS,
+}
+
+type Suit1 = "hearts" | "spades" | "diamonds" | "clubs";
+
+// enum used to differentiate different types
+// once the enum across your boundary then it's a problem.
+const suitMeaning = (suit: Suit) => {
+  if (Suit.HEARTS === suit) return "HEARTS";
+  if (Suit.SPADES === suit) return "SPADES";
+  if (Suit.DIAMONDS === suit) return "DIAMONDS";
+  if (Suit.CLUBS === suit) return "CLUBS";
+};
