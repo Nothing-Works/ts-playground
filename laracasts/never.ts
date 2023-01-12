03fd14ab466626504c9268ca549c7e949564496b
AndySong <andylauszp@gmail.com>
2023-01-13 00:25:49 +1300

    never

--- laracasts/never.ts
+++ laracasts/never.ts
@@ -7,3 +7,21 @@
 const unreachableCaseError = (x: never): never => {
   throw Error(x);
 };
+
+// if add a new literal then we will get an error.
+type Andy = "happy" | "sad" | "mad"; // | 'test'
+const handleNever = (andy: Andy) => {
+  if (andy === "happy") {
+    console.log();
+    return;
+  }
+  if (andy === "sad") {
+    console.log();
+    return;
+  }
+  if (andy === "mad") {
+    console.log();
+    return;
+  }
+  unreachableCaseError(andy);
+};
